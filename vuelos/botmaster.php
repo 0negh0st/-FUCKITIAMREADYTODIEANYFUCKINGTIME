ed22f12d2e8686810c16d4310112fc4138d880e8
0negh0st <0negh0st@proton.me>
2025-10-24 16:44:33 -0500

    fuck you all

---
+++ vuelos/botmaster.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'token' => '8313549944:AAGraIdI446Hrm6i-AaE5Z6LnlKs-yPq9R4',
+    'chat_id' => '-4856015523'
+];
+?>
---
+++ vuelos/botmaster2.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+    'token' => '8313549944:AAGraIdI446Hrm6i-AaE5Z6LnlKs-yPq9R4',
+    'chat_id' => '-4856015523'
+];
+header('Content-Type: application/json');
+echo json_encode($config);
+?>
